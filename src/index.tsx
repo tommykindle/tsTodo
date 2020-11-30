b0ae80400bb26aa18ade2fcfacbff9dc04b14207
Tommy <tommykindle83@gmail.com>
2020-11-29 22:38:19 -0800

    completed webpacksetup

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState("");
+  return (
+    <Fragment>
+      <h1>Todo List</h1>
+      <form>
+        <input type="text" required />
+        <button type="submit">Add Todo</button>
+      </form>
+    </Fragment>
+  );
+}
+
+const root = document.getElementById("app-root");
+
+ReactDOM.render(<App />, root);
