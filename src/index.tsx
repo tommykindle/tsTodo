541be6fa5e3509b9fca28b70b4110588709f31a7
Tommy <tommykindle83@gmail.com>
2020-11-30 00:06:18 -0800

    finished application

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,66 @@
 import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
+type formElm = React.FormEvent<HTMLFormElement>;
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 export default function App(): JSX.Element {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: formElm): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
   return (
     <Fragment>
       <h1>Todo List</h1>
-      <form>
-        <input type="text" required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add Todo</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}>
+            <div
+              style={{ textDecoration: todo.complete ? "line-through" : "" }}
+            >
+              {todo.text}
+            </div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {" "}
+              {todo.complete ? "Finished" : "Incomplete"}
+            </button>
+            <button type="button" onClick={() => removeTodo(index)}>
+              &times;
+            </button>
+          </Fragment>
+        ))}
+      </section>
     </Fragment>
   );
 }
